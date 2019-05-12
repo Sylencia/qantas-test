6585fc3e6f2abcfdb83a9c0ab65e49bcdb6d091f
Joseph Tran <joseph.tran.p@gmail.com>
2019-05-12 21:57:10 +1000

    Add sorting to the app, style the summary bar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from './images/qantas-logo.png'
 import styles from './App.module.scss'
 
 import { Summary } from './components/Summary'
 import { Hotel } from './components/Hotel'
 
-import { extractHotelInformation } from './Utils/HotelInfo'
+import { extractHotelInformation, sortHotels } from './Utils/HotelInfo'
 
 import data from './data/data.json'
 
 const App = () => {
+  const [sortMethod, setSortMethod] = useState('default')
   const results = data.results
+  const sortedResults = sortHotels(results, sortMethod)
 
   return (
     <div className={styles.app}>
       <img src={logo} className={styles.logo} alt="logo" />
-      <Summary numHotels={results.length} />
-      {data.results.map(hotel => {
+      <Summary numHotels={results.length} sortMethod={sortMethod} setSortMethod={setSortMethod} />
+      {sortedResults.map(hotel => {
         const hotelData = extractHotelInformation(hotel)
         return <Hotel key={hotel.id} data={hotelData} />
       })}
--- src/components/Summary/Summary.jsx
+++ src/components/Summary/Summary.jsx
@@ -2,17 +2,30 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styles from './Summary.module.scss'
 
-export const Summary = ({ numHotels }) => {
+export const Summary = ({ numHotels, sortMethod, setSortMethod }) => {
   return (
     <div className={styles.container}>
-      <div>{numHotels} hotels in Sydney</div>
-      <div>Sort by filter</div>
+      <div>
+        <span className={styles.bold}>{numHotels}</span>
+        <span className={styles.italics}> hotels in </span>
+        <span className={styles.bold}>Sydney</span>
+      </div>
+      <div>
+        {'Sort by '}
+        <select value={sortMethod} onChange={e => setSortMethod(e.target.value)}>
+          <option disabled value="default" />
+          <option value="ascending">Price low-high</option>
+          <option value="descending">Price high-low</option>
+        </select>
+      </div>
     </div>
   )
 }
 
 Summary.propTypes = {
   numHotels: PropTypes.number,
+  sortMethod: PropTypes.string.isRequired,
+  setSortMethod: PropTypes.func.isRequired,
 }
 
 Summary.defaultProps = {
