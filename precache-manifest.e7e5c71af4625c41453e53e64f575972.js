cc8377bf86eab1f86431346f7606c9acdbfd0fd2
Joseph Tran <joseph.tran.p@gmail.com>
2019-05-13 00:14:28 +1000

    Updates

---
+++ precache-manifest.e7e5c71af4625c41453e53e64f575972.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "589079d03d5972b52e2193393d465e15",
+    "url": "/qantas-test/index.html"
+  },
+  {
+    "revision": "523913f7748992710922",
+    "url": "/qantas-test/static/css/main.b80eda1b.chunk.css"
+  },
+  {
+    "revision": "cf6c05ad52ccbbacdbb4",
+    "url": "/qantas-test/static/js/2.ee5d50ee.chunk.js"
+  },
+  {
+    "revision": "523913f7748992710922",
+    "url": "/qantas-test/static/js/main.0beac705.chunk.js"
+  },
+  {
+    "revision": "3d869fded14e95946cb7",
+    "url": "/qantas-test/static/js/runtime~main.599e760d.js"
+  },
+  {
+    "revision": "a94aa0fe13d7777c2cd14edc3ca0c7ea",
+    "url": "/qantas-test/static/media/qantas-logo.a94aa0fe.png"
+  }
+]);
