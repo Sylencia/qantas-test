0704b8ac48a19fb42a5ad833c33677497c2eba25
Joseph Tran <joseph.tran.p@gmail.com>
2019-05-12 18:59:03 +1000

    Add room and free cancellation section to hotel info

--- src/components/Hotel/Information/Information.jsx
+++ src/components/Hotel/Information/Information.jsx
@@ -5,9 +5,17 @@ import { getRatingDisplay } from '../../../Utils/Rating'
 export const Information = () => (
   <div className={styles.information}>
     <div className={styles.heading}>
-      <div className={styles.name}>Primus Hotel Sydney</div>
+      <span className={styles.name}>Primus Hotel Sydney</span>
       {getRatingDisplay('self', 3.5)}
     </div>
-    <div className={styles.address}>123 Test St, Sydney</div>
+    <span className={styles.address}>123 Test St, Sydney</span>
+    <div className={styles.roomContainer}>
+      <a href="/" className={styles.room}>
+        Deluxe King Room
+      </a>
+    </div>
+    <div className={styles.cancellationContainer}>
+      <span className={styles.cancellation}>Free Cancellation</span>
+    </div>
   </div>
 )
